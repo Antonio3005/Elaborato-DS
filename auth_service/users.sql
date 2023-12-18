5cb85eb4f4a4394dac1a16051ede120ee70ecb36
anton <antonioinv1@hotmail.it>
2023-12-18 20:03:55 +0100

    commit prova

--- auth_service/users.sql
+++ auth_service/users.sql
@@ -1,43 +1,7 @@
 USE users
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `user_preferences`
---
-
-
-
---
-
---
--- Indici per le tabelle `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indici per le tabelle `user_preferences`
---
-
-
---
--- AUTO_INCREMENT per le tabelle scaricate
---
-
---
--- AUTO_INCREMENT per la tabella `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT per la tabella `user_preferences`
---
-
+CREATE TABLE IF NOT EXISTS user (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      username VARCHAR(255),
+      password VARCHAR(255)
+);
--- weather_service/user_preferences.sql
+++ weather_service/user_preferences.sql
@@ -1,17 +1,11 @@
 USE user_preferences
 
 CREATE TABLE IF NOT EXISTS  `user_preferences` (
-                                                   `id` int(11) NOT NULL,
+    `id` int AUTO_INCREMENT PRIMARY KEY,
     `user_id` varchar(255) NOT NULL,
     `city` varchar(255) NOT NULL,
     `temp_max` varchar(255) NOT NULL,
     `temp_min` varchar(255) NOT NULL,
     `rain_amount` varchar(255) NOT NULL,
     `snow_presence` varchar(255) NOT NULL
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-ALTER TABLE `user_preferences`
-    ADD PRIMARY KEY (`id`);
-ALTER TABLE `user_preferences`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
+    );
