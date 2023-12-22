fd4b9bab3acd2e977c59dd4cb424ce78d1545380
antonio3005 <antonioinv1@hotmail.it>
2023-12-22 17:15:25 +0100

    adas

--- api_service/api.py
+++ api_service/api.py
@@ -26,6 +26,44 @@ class UserPreferences(db.Model):
 with app.app_context():
     db.create_all()
 
+def get_iata(city):
+
+    url = 'https://api.tequila.kiwi.com/locations/query'
+    headers = {
+        'accept': 'application/json',
+        'apikey': 'qLsLVL8oCHp3riP0lbb3PTcz0TNc3r-Y'
+    }
+
+    params = {
+        'term': city,
+        'locale': 'it-IT',
+        'location_types': 'city',
+        'limit': 10,
+        'active_only': True
+    }
+
+    response = requests.get(url, params=params, headers=headers)
+
+    if response.status_code == 200:
+        # Analizza la risposta JSON
+        try:
+            data = response.json()
+        # Assicurati che l'array "locations" sia presente e non vuoto
+            if "locations" in data and data["locations"]:
+                # Estrai il valore associato alla chiave "code"
+                iata = data["locations"][0]["code"]
+                print(iata)
+            else:
+                print("Array 'locations' vuoto o assente nella risposta dell'API.")
+        except json.JSONDecodeError:
+        print("Errore nella decodifica della risposta JSON.")
+
+    else:
+        # Se la richiesta non è andata a buon fine, stampa il codice di stato
+        print(f"Errore nella richiesta. Codice di stato: {response.status_code}")
+
+    return iata
+
 @app.route('/weather', methods=['GET'])
 def get_weather():
 
@@ -33,71 +71,73 @@ def get_weather():
     print(subscription)
 
 
-    
+
+
+
     import requests
 
-url = 'https://api.tequila.kiwi.com/locations/query'
-headers = {
-    'accept': 'application/json',
-    'apikey': 'qLsLVL8oCHp3riP0lbb3PTcz0TNc3r-Y'
-}
+    url = 'https://api.tequila.kiwi.com/locations/query'
+    headers = {
+        'accept': 'application/json',
+        'apikey': 'qLsLVL8oCHp3riP0lbb3PTcz0TNc3r-Y'
+    }
 
-params = {
-    'term': 'roma',
-    'locale': 'it-IT',
-    'location_types': 'city',
-    'limit': 10,
-    'active_only': True
-}
+    params = {
+        'term': 'roma',
+        'locale': 'it-IT',
+        'location_types': 'city',
+        'limit': 10,
+        'active_only': True
+    }
 
-response = requests.get(url, params=params, headers=headers)
+    response = requests.get(url, params=params, headers=headers)
 
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-else:
-    print(f"Error: {response.status_code}, {response.text}")
-
-import requests
-
-url = 'https://api.tequila.kiwi.com/v2/search'
-headers = {
-    'accept': 'application/json',
-    'apikey': 'qLsLVL8oCHp3riP0lbb3PTcz0TNc3r-Y'
-}
-
-params = {
-    'fly_from': fly_from,
-    'fly_to': 'PRG',
-    'date_from': '01/04/2024',
-    'date_to': '03/04/2024',
-    'return_from': '04/04/2024',
-    'return_to': '06/04/2024',
-    'adults': 1,
-    'adult_hand_bag': 1,
-    'partner_market': 'it',
-    'price_from': 10,
-    'price_to': 5000,
-    'vehicle_type': 'aircraft',
-    'sort': 'price',
-    'limit': 50
-}
-
-response = requests.get(url, params=params, headers=headers)
-
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-else:
-    print(f"Error: {response.status_code}, {response.text}")
-
-    flights = requests.get(url, params=params, headers=headers)
-
-    if flights.status_code == 200:
+    if response.status_code == 200:
         data = response.json()
         print(data)
     else:
-    print(f"Error: {flights.status_code}, {flights.text}")
+        print(f"Error: {response.status_code}, {response.text}")
+
+    import requests
+
+    url = 'https://api.tequila.kiwi.com/v2/search'
+    headers = {
+        'accept': 'application/json',
+        'apikey': 'qLsLVL8oCHp3riP0lbb3PTcz0TNc3r-Y'
+    }
+
+    params = {
+        'fly_from': fly_from,
+        'fly_to': 'PRG',
+        'date_from': '01/04/2024',
+        'date_to': '03/04/2024',
+        'return_from': '04/04/2024',
+        'return_to': '06/04/2024',
+        'adults': 1,
+        'adult_hand_bag': 1,
+        'partner_market': 'it',
+        'price_from': 10,
+        'price_to': 5000,
+        'vehicle_type': 'aircraft',
+        'sort': 'price',
+        'limit': 50
+    }
+
+    response = requests.get(url, params=params, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+
+        flights = requests.get(url, params=params, headers=headers)
+
+        if flights.status_code == 200:
+            data = response.json()
+            print(data)
+        else:
+        print(f"Error: {flights.status_code}, {flights.text}")
 
     #if not city:
     #    return jsonify({'error': 'Specificare una città nella richiesta'}), 400
