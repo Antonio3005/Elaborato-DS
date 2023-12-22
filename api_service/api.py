7652a16be0f5ac8c89caf0702638a76dcfcbfa79
antonio3005 <antonioinv1@hotmail.it>
2023-12-22 10:59:50 +0100

    adas

--- api_service/api.py
+++ api_service/api.py
@@ -6,6 +6,54 @@ app = Flask(__name__)
 # Chiave API di OpenWeatherMap
 api_key = 'f5bea9a7cd302abdca838d809883d3eb'
 
+import requests
+
+url = 'https://api.tequila.kiwi.com/v2/search'
+headers = {
+    'accept': 'application/json',
+    'apikey': 'D-ljrLyhPOeM1-eWQWcVliV9mBNListf'
+}
+
+params = {
+    'fly_from': 'FRA',
+    'fly_to': 'PRG',
+    'date_from': '01/04/2024',
+    'date_to': '03/04/2024',
+    'return_from': '04/04/2024',
+    'return_to': '06/04/2024',
+    'nights_in_dst_from': 2,
+    'nights_in_dst_to': 3,
+    'max_fly_duration': 20,
+    'ret_from_diff_city': True,
+    'ret_to_diff_city': True,
+    'one_for_city': 0,
+    'one_per_date': 0,
+    'adults': 2,
+    'children': 2,
+    'selected_cabins': 'C',
+    'mix_with_cabins': 'M',
+    'adult_hold_bag': '1,0',
+    'adult_hand_bag': '1,1',
+    'child_hold_bag': '2,1',
+    'child_hand_bag': '1,1',
+    'only_working_days': False,
+    'only_weekends': False,
+    'partner_market': 'us',
+    'max_stopovers': 2,
+    'max_sector_stopovers': 2,
+    'vehicle_type': 'aircraft',
+    'limit': 100
+}
+
+response = requests.get(url, params=params, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error: {response.status_code}, {response.text}")
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
